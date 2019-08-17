e1d465ef46c0c264c241751cb33e15135de874e7
minlwin <lwin.zawmin@gmail.com>
2019-08-17 11:59:02 +0630

    Update Views

--- mini-pos/src/com/jdc/pos/controller/PosView.java
+++ mini-pos/src/com/jdc/pos/controller/PosView.java
@@ -1,6 +1,57 @@
 package com.jdc.pos.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class PosView {
 
+    @FXML
+    private ComboBox<?> category;
+
+    @FXML
+    private TextField item;
+
+    @FXML
+    private TableView<?> items;
+
+    @FXML
+    private Label headTotal;
+
+    @FXML
+    private TableView<?> orders;
+
+    @FXML
+    private Label subTotal;
+
+    @FXML
+    private Label tax;
+
+    @FXML
+    private Label total;
+
+    @FXML
+    void clear(MouseEvent event) {
+
+    }
+
+    @FXML
+    void clearSearch(ActionEvent event) {
+
+    }
+
+    @FXML
+    void delete(MouseEvent event) {
+
+    }
+
+    @FXML
+    void paid(MouseEvent event) {
+
+    }
 
 }
--- mini-pos/src/com/jdc/pos/controller/SaleHistory.java
+++ mini-pos/src/com/jdc/pos/controller/SaleHistory.java
@@ -1,6 +1,32 @@
 package com.jdc.pos.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class SaleHistory {
 
+    @FXML
+    private ComboBox<?> category;
+
+    @FXML
+    private TextField item;
+
+    @FXML
+    private DatePicker dateFrom;
+
+    @FXML
+    private DatePicker dateTo;
+
+    @FXML
+    private TableView<?> table;
+
+    @FXML
+    void clear(ActionEvent event) {
+
+    }
 
 }
